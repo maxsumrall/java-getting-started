2c1966691fa6fd26c51154f837197d4f742ae203
Max Sumrall <max.sumrall@neotechnology.com>
2016-11-08 16:26:16 +0100

    looks like it's working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
-import com.heroku.sdk.jdbc.DatabaseUrl;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.neo4j.driver.v1.AccessMode;
@@ -19,20 +15,22 @@ import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.StatementResult;
 import org.neo4j.driver.v1.Transaction;
 
+import static org.neo4j.driver.v1.Values.parameters;
 import static spark.Spark.get;
 import static spark.Spark.port;
+import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 
 public class Main
 {
+    public static Driver driver = GraphDatabase
+            .driver( "bolt+routing://ec2-54-170-252-11.eu-west-1.compute.amazonaws" + ".com:26000",
+                    AuthTokens.basic( "neo4j", "max" ) );
 
     public static void main( String[] args )
     {
-
         port( Integer.valueOf( System.getenv( "PORT" ) ) );
         staticFileLocation( "/public" );
-
-        get( "/hello", ( req, res ) -> "Hello World" );
 
         get( "/", ( request, response ) ->
         {
@@ -42,64 +40,61 @@ public class Main
             return new ModelAndView( attributes, "index.ftl" );
         }, new FreeMarkerEngine() );
 
-        get( "/neo4j", ( req, res ) ->
+        get( "/photos", ( req, res ) ->
         {
-            String outMessage = "Nothing found in DB";
-            Driver driver = GraphDatabase.driver( "bolt://ec2-54-170-252-11.eu-west-1.compute.amazonaws.com:26000",
-                    AuthTokens.basic( "neo4j","max" ));
+            String bookmark = req.queryParams( "bookmark" );
+            System.out.println( "bookmark = " + bookmark );
+
+            List<Object> attributes = new ArrayList<>();
+
             try ( Session session = driver.session( AccessMode.READ ) )
             {
-                Transaction transaction = session.beginTransaction();
-                StatementResult run = transaction.run( "MATCH (n) RETURN n" );
-                Record single = run.single();
-                outMessage = single.toString();
+                Transaction transaction = session.beginTransaction(bookmark);
+                StatementResult run = transaction.run(
+                        "MATCH (n:Picture) RETURN n.uri AS uri, SIZE((n)<-[:FOR]-()) AS votes" );
+                while ( run.hasNext() )
+                {
+                    Record next = run.next();
+                    attributes.add( next.asMap() );
+                }
                 transaction.success();
             }
-            driver.close();
-            return outMessage;
-        } );
 
-        get( "/db", ( req, res ) ->
-        {
-            Connection connection = null;
-            Map<String,Object> attributes = new HashMap<>();
-            try
-            {
-                connection = DatabaseUrl.extract().getConnection();
+            Map<String, Object> params = new HashMap<>(  );
+            params.put( "photos", attributes );
 
-                Statement stmt = connection.createStatement();
-                stmt.executeUpdate( "CREATE TABLE IF NOT EXISTS ticks (tick timestamp)" );
-                stmt.executeUpdate( "INSERT INTO ticks VALUES (now())" );
-                ResultSet rs = stmt.executeQuery( "SELECT tick FROM ticks" );
-
-                ArrayList<String> output = new ArrayList<String>();
-                while ( rs.next() )
-                {
-                    output.add( "Read from DB: " + rs.getTimestamp( "tick" ) );
-                }
-
-                attributes.put( "results", output );
-                return new ModelAndView( attributes, "db.ftl" );
-            }
-            catch ( Exception e )
-            {
-                attributes.put( "message", "There was an error: " + e );
-                return new ModelAndView( attributes, "error.ftl" );
-            }
-            finally
-            {
-                if ( connection != null )
-                {
-                    try
-                    {
-                        connection.close();
-                    }
-                    catch ( SQLException e )
-                    {
-                    }
-                }
-            }
+            return new ModelAndView( params, "photos.ftl" );
         }, new FreeMarkerEngine() );
+
+        post("/photos", (req, res) ->
+        {
+            String currentUser = "Alistair";
+
+            String myVote = req.queryParams( "photo" );
+            System.out.println( "I voted for = " + myVote );
+
+            String bookmark;
+            try ( Session session = driver.session( AccessMode.WRITE ) )
+            {
+                try(Transaction transaction = session.beginTransaction())
+                {
+
+                    transaction
+                            .run( "MATCH (n:Picture {uri: {id}}), (person:Person {name: {name}}) " +
+                                            "CREATE (person)-[:CAST]->(vote:Vote)-[:FOR]->(n)",
+                                    parameters( "id", myVote, "name", currentUser ) ).consume();
+                    transaction.success();
+                }
+
+                bookmark = session.lastBookmark();
+            }
+
+            System.out.println( "bookmark = " + bookmark );
+
+
+            res.redirect( "/photos?bookmark=" + bookmark );
+            return "";
+        });
 
     }
 
---
+++ src/main/resources/spark/template/freemarker/photos.ftl
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<#include "header.ftl">
+</head>
+
+<body>
+
+<#include "nav.ftl">
+
+<div class="jumbotron text-center">
+    <div class="container">
+        <a href="/" class="lang-logo">
+            <img src="/lang-logo.png">
+        </a>
+        <h1>Making Neo4j Drivers Great - here lie the photos.</h1>
+    </div>
+</div>
+<div class="container">
+
+    <ul>
+    <#list photos as photo>
+        <li>
+            <form action="/photos" method="POST" name="votingForm">
+                <img src="${photo.uri}"/>
+                <Strong>${photo.votes}</Strong>
+                <label for="vote">Vote</label>
+                <input type="hidden" value="${photo.uri}" name="photo"/>
+                <input type="submit" name="Vote" value="Vote"/>
+            </form>
+        </li>
+    </#list>
+
+    </ul>
+
+</div>
+
+
+</body>
+</html>
